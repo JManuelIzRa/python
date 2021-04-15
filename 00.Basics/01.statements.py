fc8dbcd457ebf4408d94825aa9bdb3fda9aa70d5
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-04-15 23:35:11 +0200

    statements

---
+++ 00.Basics/01.statements.py
@@ -0,0 +1,8 @@
+help(print) #Tipico man o --help
+
+print("This program calculates the average of two numbers");#El punto y como está admitido, pero la convención es usar el salto de línea
+print("The numbers are 4 and 8")#python es un lenguaje sensible al sangrado (indentation), un simple espacio puede indicar que una función está dentro de otra
+
+#Es key sensistive, una funcion escrita con mayuscula no hara las funciones de la funcion original
+
+print("The average is ", (4+8)/2)#pytohn sigue la jerarquía matemática, es decir si no ponemos parentesis primero haria 8/2 y luego 4+4
