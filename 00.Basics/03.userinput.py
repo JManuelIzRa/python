b08de4b8dc50b6be4f848ab267a12974ebe6bb06
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-04-16 00:19:53 +0200

    User input

---
+++ 00.Basics/03.userinput.py
@@ -0,0 +1,18 @@
+number1=input("Please enter the first number: ")
+
+print(number1)
+
+number2=input("Please enter the first number: ")
+
+print(number2)
+
+print(number1+number2)#Nos da 48 porque cada vez que llamamos al user input el valor devuelto es un string, entonces lo que realmente estamos haciendo es concatenar number1 y number2
+
+type(number1)#Devuelve el tipo de la variable pasada como argumento
+
+first_name="JoseManuel"
+last_name="Iz"
+
+print("Your e-mail addess is",first_name+"."+last_name+"@gmail.com")
+
+
