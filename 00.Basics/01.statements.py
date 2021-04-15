5f87975b714b018aef2355ba1db4ead300d93103
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-04-16 00:20:47 +0200

    Aclaricion ;

--- 00.Basics/01.statements.py
+++ 00.Basics/01.statements.py
@@ -1,6 +1,6 @@
 help(print) #Tipico man o --help
 
-print("This program calculates the average of two numbers");#El punto y como está admitido, pero la convención es usar el salto de línea
+print("This program calculates the average of two numbers");#El punto y como está admitido, pero la convención es usar el salto de línea, de hecho si lo ponemos nos da un warning diciendo que es innecesaria
 print("The numbers are 4 and 8")#python es un lenguaje sensible al sangrado (indentation), un simple espacio puede indicar que una función está dentro de otra
 
 #Es key sensistive, una funcion escrita con mayuscula no hara las funciones de la funcion original
