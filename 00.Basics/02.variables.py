5a5082c4b23d1ba0017fc8875a8d3940f919746e
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-04-16 00:19:38 +0200

    Variables

---
+++ 00.Basics/02.variables.py
@@ -0,0 +1,12 @@
+#las variables en python no necesitan especificar el tipo
+
+age=20
+print(age)
+
+print("Tengo",age,"años")
+
+num1=4
+num2=8
+
+print("The numbers are", num1, "and", num2)
+print("The average is: ",(num1+num2)/2)
