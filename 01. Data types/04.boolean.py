ef7751612c4357fedbd04418311dd0c9fa93c2e2
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-04-20 12:08:54 +0200

    Boolean

---
+++ 01. Data types/04.boolean.py
@@ -0,0 +1,16 @@
+myBoolean = True #If we type "True" it is a string
+
+print(type(myBoolean))
+
+num1 = 8
+num2 = 5
+
+print("Comparacion num1 < num2: ",num1,"<",num2,num1 < num2)
+
+print("Comparacion num1 > num2: ",num1,">",num2,num1 > num2)
+
+print("Comparacion num1 == num2: ",num1,"==",num2,num1 == num2)
+
+print("Comparacion num1 != num2: ",num1,"!=",num2,num1 != num2)
+
+#Operadores logicos > < == != <= >=
