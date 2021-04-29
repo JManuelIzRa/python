2ab4d7e4a1d6649a9be5a86baabb30de4e478d17
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-04-29 11:40:00 +0200

    Funciones

--- 02. Conditionals, Loops, Functions/07.functions.py
+++ 02. Conditionals, Loops, Functions/07.functions.py
@@ -3,4 +3,20 @@ def say_hello(person):
     print("Hello ", person,", how are you?")
     
 
+def fahr2celsius(fahr):
+    celsius = (5 * (fahr - 32)) / 9
+    return celsius
+
+def people(person1, person2 = "Por defecto"):
+    print("Hola",person1,"y",person2)
+
+
 say_hello("Pepe")
+
+print("Celsius: ",fahr2celsius(100))
+
+print("Kelvin: ",fahr2celsius(100)+273)
+
+people("Pepe")
+
+people("Pepe", "Pepa")
