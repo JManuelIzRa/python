87681cd6264c320f59b5c403a034d08607af6a50
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-05-25 21:00:12 +0200

    File handling

---
+++ 04.File_Handling/00. File_handling.py
@@ -0,0 +1,19 @@
+import os
+
+cwd = os.getcwd()  # Obtiene el directorio de trabajo actual (cwd)
+files = os.listdir(cwd)  # Obtiene todos los archivos del directorio
+print("Files in %r: %s" % (cwd, files))
+
+# Si muestra un error de que no encuentra el fichero y este existe, puede ser
+# porque estamos trabajando en un directorio distinto, para cambiar de directorio
+# usamos la funcion os.chdir(), poniendo r'' para que la cadena que pongamos sea
+# interpretada literalmente
+os.chdir(r'D:\Documentos D\Programación\Github\Python3\python\04.File_Handling')
+
+# Por defecto la flag es la r de lectura si no ponemos nada
+# f = open("test.txt", "r")
+f = open("test.txt")
+
+print(f.read()) # Imprime todas las lineas del fichero
+
+f.close()
---
+++ 04.File_Handling/01.File_handling2.py
@@ -0,0 +1,19 @@
+import os
+
+cwd = os.getcwd()  # Obtiene el directorio de trabajo actual (cwd)
+files = os.listdir(cwd)  # Obtiene todos los archivos del directorio
+print("Archivos en %r: %s" % (cwd, files))
+
+os.chdir(r'D:\Documentos D\Programación\Github\Python3\python\04.File_Handling')
+
+# La flag w es de escritura y sobreesccribe el contenido del fichero.
+f = open("write.txt","w")
+
+f.write("Esto es una prueba de escritura en un fichero.")
+
+f.close()
+
+# La flag a es para añadir a un fichero.
+f = open("write.txt","a")
+
+f.write("\nEste texto va a ser añadido")
