1728b79814111bcff43acb67f99d23a6f2d7f5d8
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2022-05-16 23:38:36 +0200

    Day 1. Beginner. Input Output

---
+++ 08.100DaysOfPython/Day01/main.py
@@ -0,0 +1,16 @@
+# This is the final exercise for that day, it is too easy for me, so i decided to skip all the videos
+
+#1. Create a greeting for your program
+print("Welcome to IZRA's band name generator")
+
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?\n")
+
+#3. Ask the user for the name of a pet
+pet = input("Which is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show their band name.
+print(f"You band name could be {city+pet}")
+
+#5. Make sure the input cursor shows on a new line
+
